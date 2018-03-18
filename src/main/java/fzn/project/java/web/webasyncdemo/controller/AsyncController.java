d0ba3a0e400d98dbd5378d64b7ae7b07d7d1d3a5
FzN <fzn0268@hotmail.com>
2018-03-18 17:46:39 +0800

    cross origin

--- src/main/java/fzn/project/java/web/webasyncdemo/controller/AsyncController.java
+++ src/main/java/fzn/project/java/web/webasyncdemo/controller/AsyncController.java
@@ -1,11 +1,13 @@
 package fzn.project.java.web.webasyncdemo.controller;
 
-import lombok.extern.java.Log;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseBodyEmitter;
@@ -13,25 +15,29 @@ import org.springframework.web.servlet.mvc.method.annotation.ResponseBodyEmitter
 import java.io.IOException;
 import java.util.concurrent.TimeUnit;
 
-@Log
+@Slf4j
 @Controller
+@CrossOrigin
 public class AsyncController {
 
     @Autowired
     private AsyncHelper asyncHelper;
 
     @GetMapping(path = "/streaming")
-    public ResponseBodyEmitter streaming(@RequestParam long eventNumber, @RequestParam long intervalSec) throws IOException, InterruptedException {
+    public ResponseEntity<ResponseBodyEmitter> streaming(@RequestParam long eventNumber, @RequestParam long intervalSec) throws IOException, InterruptedException {
         log.info("Start get.");
 
         ResponseBodyEmitter emitter = new ResponseBodyEmitter();
         asyncHelper.streaming(emitter, eventNumber, intervalSec);
 
         log.info("End get.");
-        return emitter;
+        return ResponseEntity.ok()
+                .contentType(MediaType.TEXT_PLAIN)
+                .header("X-Content-Type-Options", "nosniff")
+                .body(emitter);
     }
 
-    @Log
+    @Slf4j
     @Component
     public static class AsyncHelper {
         // ...
@@ -41,9 +47,9 @@ public class AsyncController {
 
             for (long i = 1; i <= eventNumber; i++) {
                 TimeUnit.SECONDS.sleep(intervalSec);
-                emitter.send("msg" + i + "\r\n", MediaType.TEXT_PLAIN);
+                emitter.send("msg" + i + "\r\n");
+                log.info("Sent {}", i);
             }
-
             emitter.complete();
 
             log.info("End Async processing.");
